78357a1fb2acf535877f78576bd6cf9357280cfe
fsutawanir <fannyirawans@gmail.com>
2019-09-02 17:20:02 +0700

    change gpt.js url

--- index.js
+++ index.js
@@ -1,101 +1,92 @@
-'use strict';
+"use strict";
 
-import BannerAd from './components/BannerAd';
+import BannerAd from "./components/BannerAd";
 
 export default {
-    sizes: {
-        banner: [
-            [1200, 600],
-            [1200, 400],
-            [300, 250],
-            [300, 300]
-        ],
-        rectangle: [
-            [300, 250],
-            [300, 300],
-            [300, 600]
-        ]
-    },
-    /**
-     * Install DoubleClick Plugin
-     * @param Vue
-     * @param options
-     */
-    install(Vue, options) {
-        Vue.component('google-ad', BannerAd);
+  sizes: {
+    banner: [[1200, 600], [1200, 400], [300, 250], [300, 300]],
+    rectangle: [[300, 250], [300, 300], [300, 600]]
+  },
+  /**
+   * Install DoubleClick Plugin
+   * @param Vue
+   * @param options
+   */
+  install(Vue, options) {
+    Vue.component("google-ad", BannerAd);
 
-        this.init(options, Vue)
-    },
-    /**
-     * Initialize google tag manager
-     * @param options
-     */
-    init(options, Vue) {
-        window.googletag = {};
-        let googledfpviewable = 0;
-        googletag.cmd = googletag.cmd || [];
-        let gads = document.createElement('script');
-        gads.async = true;
-        gads.type = 'text/javascript';
-        let useSSL = 'https:' == document.location.protocol;
-        gads.src = (useSSL ? 'https:' : 'http:') +
-            '//www.googletagservices.com/tag/js/gpt.js';
-        let node = document.getElementsByTagName('script')[0];
-        node.parentNode.insertBefore(gads, node);
+    this.init(options, Vue);
+  },
+  /**
+   * Initialize google tag manager
+   * @param options
+   */
+  init(options, Vue) {
+    window.googletag = {};
+    let googledfpviewable = 0;
+    googletag.cmd = googletag.cmd || [];
+    let gads = document.createElement("script");
+    gads.async = true;
+    gads.type = "text/javascript";
+    let useSSL = "https:" == document.location.protocol;
+    gads.src =
+      (useSSL ? "https:" : "http:") +
+      "//securepubads.g.doubleclick.net/tag/js/gpt.js";
+    let node = document.getElementsByTagName("script")[0];
+    node.parentNode.insertBefore(gads, node);
 
-        googletag.cmd.push(() => {
-            this.extractOptions(options, Vue);
+    googletag.cmd.push(() => {
+      this.extractOptions(options, Vue);
 
-            googletag.pubads().enableSingleRequest();
-            //googletag.pubads().enableSyncRendering();
-            googletag.pubads().collapseEmptyDivs();
-            // googletag.pubads().disableInitialLoad();
-            googletag.enableServices();
-            //googletag.display('dynamicload');
-            // googletag.pubads().refresh();
-        });
+      googletag.pubads().enableSingleRequest();
+      //googletag.pubads().enableSyncRendering();
+      googletag.pubads().collapseEmptyDivs();
+      // googletag.pubads().disableInitialLoad();
+      googletag.enableServices();
+      //googletag.display('dynamicload');
+      // googletag.pubads().refresh();
+    });
+  },
 
-    },
+  /**
+   * Extract plugin options
+   * @param options
+   */
+  extractOptions(options, Vue) {
+    options.sizes = Object.assign(this.sizes, options.sizes);
+    options.mappings = this.prepareMappings(options.mappings);
 
-    /**
-     * Extract plugin options
-     * @param options
-     */
-    extractOptions(options, Vue) {
-        options.sizes = Object.assign(this.sizes, options.sizes);
-        options.mappings = this.prepareMappings(options.mappings);
+    if (!options.default_size) {
+      options.default_size = "banner";
+    }
 
-        if (!options.default_size) {
-            options.default_size = "banner";
-        }
+    Vue.prototype.$doubleclick = options;
+  },
 
-        Vue.prototype.$doubleclick = options;
-    },
+  /**
+   * Prepare mapped sizes
+   * @param mappings
+   * @returns {*}
+   */
+  prepareMappings(mappings) {
+    Object.keys(mappings).forEach(key => {
+      mappings[key] = this.mapSize(mappings[key]);
+    });
 
-    /**
-     * Prepare mapped sizes
-     * @param mappings
-     * @returns {*}
-     */
-    prepareMappings(mappings) {
-        Object.keys(mappings).forEach((key) => {
-            mappings[key] = this.mapSize(mappings[key])
-        });
+    return mappings;
+  },
+  /**
+   * Map single size
+   * @param mapping
+   * @returns {*}
+   */
+  mapSize(mapping) {
+    let mapper = googletag.sizeMapping();
 
-        return mappings;
-    },
-    /**
-     * Map single size
-     * @param mapping
-     * @returns {*}
-     */
-    mapSize(mapping) {
-        let mapper = googletag.sizeMapping();
+    mapping.forEach(map => {
+      mapper.addSize(map.window, map.sizes);
+    });
 
-        mapping.forEach((map) => {
-            mapper.addSize(map.window, map.sizes)
-        });
-
-        return mapper.build();
-    },
-}
+    return mapper.build();
+  }
+};
